ae7b64192bd89cbeb2c528691415567b81590fa5
Quentin Dercon <quentindercon@gmail.com>
2022-09-20 21:59:55 +0100

    first commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+    $('input[type="checkbox"]').bind('change', function() {
+        if($(this).is(':checked')) {
+            $('.altmetric-embed').delay(100).fadeIn('fast'); // i.e., toggle altmetric badges on
+        } else {
+            $('.altmetric-embed').hide();
+        }
+    });
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.description').click(function() {
+        $(this).parent().parent().find(".description.hidden").toggleClass('open');
     });
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
